72f920d87185f6bbfbdf80636ef9ad7948eff1a0
seiji <51401797+seijidev@users.noreply.github.com>
2024-06-05 20:06:43 -0800

    Add html and css lsps to init.lua

--- init.lua
+++ init.lua
@@ -542,6 +542,10 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        asm_lsp = {},
+        asmfmt = {},
+        html = {},
+        cssls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
