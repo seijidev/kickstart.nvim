1ce75003f0f4dfd216287acaced112cc772af550
seiji <51401797+seijidev@users.noreply.github.com>
2024-06-06 19:35:16 -0800

    Install html formatter through Mason and set html to use it in init.lua

--- init.lua
+++ init.lua
@@ -634,6 +634,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        html = { 'htmlbeautifier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
